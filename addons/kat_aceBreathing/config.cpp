afa489fea2f2b19dfdc81f082b7598e06889ef9d
Katalam <goerss99@gmail.com>
2018-12-01 14:06:54 +0100

    Adds breathing field manual

--- addons/kat_aceBreathing/config.cpp
+++ addons/kat_aceBreathing/config.cpp
@@ -27,6 +27,18 @@ class CfgPatches
     count = COUNT; \
 };
 
+class CfgHints	{
+	class KAT_Hint	{
+		class KAT_Breathing	{
+			displayName = $STR_kat_aceBreathing_hint_name;
+			description = $STR_kat_aceBreathing_hint_desc;
+      tip = $STR_kat_aceBreathing_hint_tip;
+      arguments[] = {};
+			image = "kat_aceAirway\images\larynx.paa";
+		};
+	};
+};
+
 class CfgFunctions {
 	class kat_aceBreathing {
     file = "kat_aceBreathing\functions";
