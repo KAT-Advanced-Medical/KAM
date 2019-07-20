dd6d30925a8ee4b6d8be5b092c0b1c1a595b04db
Katalam <goerss99@gmail.com>
2019-07-20 10:13:06 +0200

    Create ACE_Medical_Injuries.hpp

---
+++ addons/airway/ACE_Medical_Injuries.hpp
@@ -0,0 +1,28 @@
+class ACE_Medical_Injuries {
+    class wounds {
+        class Abrasion {
+            internal = 0;
+        };
+        class Avulsion {
+            internal = 1;
+        };
+        class Contusion {
+            internal = 0;
+        };
+        class Crush {
+            internal = 0;
+        };
+        class Cut {
+            internal = 0;
+        };
+        class Laceration {
+            internal = 0;
+        };
+        class VelocityWound {
+            internal = 1;
+        };
+        class PunctureWound {
+            internal = 0;
+        };
+    };
+};
