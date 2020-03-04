9646832cd3c76e7e652fa6043c7debe463cf74a7
KP <conrimaceogain@gmail.com>
2020-03-03 23:55:39 -0800

    Resolve conflicting macro imports
    ACE and KAT script_macros.hpp are mostly identical with some small diffs

--- addons/airway/script_component.hpp
+++ addons/airway/script_component.hpp
@@ -16,7 +16,6 @@
 
 #include "\x\kat\addons\main\script_macros.hpp"
 #include "\z\ace\addons\medical_engine\script_macros_medical.hpp"
-#include "\z\ace\addons\main\script_macros.hpp"
 
 #include "\a3\ui_f\hpp\defineResincl.inc"
 #include "\a3\ui_f\hpp\defineDIKCodes.inc"
