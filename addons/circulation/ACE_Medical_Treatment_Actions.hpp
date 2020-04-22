4529d44a2910225c586eae12ba41baf7f97db6c1
YetheSamartaka <55753928+YetheSamartaka@users.noreply.github.com>
2020-04-22 17:39:38 +0200

    Defib and AED-X settings per medical class
    Add settings for defibrillator and AED-X and for each option who can use it.
    Corrected action texts for Defib and AED-X

---
+++ addons/circulation/ACE_Medical_Treatment_Actions.hpp
@@ -0,0 +1,80 @@
+class ACE_Medical_Treatment_Actions {
+    class FieldDressing;
+    class Morphine;
+    class CheckPulse;
+    class CPR;
+
+    class Painkillers: Morphine {
+        displayName = CSTRING(Inject_Box_Painkillers);
+        displayNameProgress = CSTRING(Using);
+        allowedSelections[] = {"Head"};
+        items[] = {};
+        callbackSuccess = QUOTE([ARR_5('kat_Painkiller', _medic, _patient, _bodyPart, 'Painkillers')] call FUNC(removeItemfromMag); [_patient] call FUNC(wrongBloodTreatment););
+        condition = "[_medic, 'kat_Painkiller'] call ace_common_fnc_hasMagazine || [_patient, 'kat_Painkiller'] call ace_common_fnc_hasMagazine";
+        litter[] = {};
+        icon = QPATHTOF(ui\icon_painkillers_action.paa);
+    };
+    class CheckDogtags: checkPulse {
+        displayName = CSTRING(DogTag);
+        displayNameProgress = CSTRING(DogTag_Action);
+        treatmentTime = 2;
+        allowedSelections[] = {"Head"};
+        allowSelfTreatment = 1;
+        callbackSuccess = "[_medic, _patient] call ace_dogtags_fnc_checkDogtag";
+        condition = "true";
+    };
+    class CheckBloodPressure: CheckPulse { // Remove the ability to check blood pressure at the head
+        allowedSelections[] = {"LeftArm", "RightArm", "LeftLeg", "RightLeg"};
+    };
+
+    #include "Blood_Medical.hpp"
+
+
+//For AED
+    class Defibrillator: CPR {
+		displayName = CSTRING(Defib_Action_Use);
+        items[] = {};
+        treatmentTime = 10;
+        callbackProgress = QUOTE(call FUNC(AED_sound));
+        condition = QUOTE((_patient getVariable [ARR_2(QQGVAR(X), false)]) || [ARR_2(_medic, 'adv_aceCPR_AED')] call ace_common_fnc_hasItem || [ARR_2(_medic, 'kat_X_AED')] call ace_common_fnc_hasItem);
+        medicRequired = 1;
+        icon = QPATHTOF(ui\X_Series-Device_W.paa);
+    };
+ //For remove defib   
+    class Remove_X_Defibrillator: Defibrillator {
+        displayName = CSTRING(X_Action_Remove);
+		displayNameProgress = CSTRING(X_Remove_Action_Progress);
+        items[] = {};
+        condition = QUOTE(_patient getVariable [ARR_2(QQGVAR(X), true)]);
+        treatmentTime = 5;
+        medicRequired = 0;
+        callbackProgress = "";
+        callbackSuccess = QUOTE([ARR_2(_medic, _patient)] call FUNC(returnAED_X));
+        icon = QPATHTOF(ui\X_Series-Device_W.paa);
+    };
+//For AED-X
+    class Defibrillator_AED_X: CPR {
+		displayName = CSTRING(AED_X_Action_Use);
+        items[] = {"kat_X_AED"};
+        treatmentTime = 10;
+        callbackProgress = QUOTE(call FUNC(AED_sound));
+        condition = QUOTE((_patient getVariable [ARR_2(QQGVAR(X), false)]) || [ARR_2(_medic, 'adv_aceCPR_AED')] call ace_common_fnc_hasItem || [ARR_2(_medic, 'kat_X_AED')] call ace_common_fnc_hasItem);
+        medicRequired = QGVAR(medLvl_AED_X);
+        icon = QPATHTOF(ui\X_Series-Device_W.paa);
+    };
+    class X_Defibrillator_AED_X: CPR {
+        displayName = CSTRING(X_Action_Use);
+        displayNameProgress = CSTRING(X_Action_Progress);
+        items[] = {"kat_X_AED"};
+        condition = QUOTE(!(_patient getVariable [ARR_2(QQGVAR(X), false)]));
+        treatmentTime = 5;
+        consumeItem = 1;
+        medicRequired = QGVAR(medLvl_AED_X);
+        callbackProgress = "";
+        callbackStart = "";
+        callbackFailure = "";
+        callbackSuccess = QFUNC(treatmentAdvanced_X);
+        animationCaller = "AinvPknlMstpSnonWnonDnon_medic3";
+        icon = QPATHTOF(ui\X_Series-Device_W.paa);
+    };
+};
