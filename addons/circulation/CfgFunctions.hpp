06bd1c74c3fc67d41fae347fb068894817f88369
Tom <tom.klug02@gmail.com>
2021-09-15 00:22:10 +0200

    Update CfgFunctions.hpp
    only white spaces changed

--- addons/circulation/CfgFunctions.hpp
+++ addons/circulation/CfgFunctions.hpp
@@ -1,24 +1,24 @@
 class CfgFunctions {
-    class overwrite_dotags {
-        tag = "ace_dogtags";
-        class ace_dogtags {
-            class getDogtagData {
-                file = QPATHTOF(functions\fnc_getDogtagData.sqf);
+	class overwrite_dotags {
+    	tag = "ace_dogtags";
+    	class ace_dogtags {
+        	class getDogtagData {
+            	file = QPATHTOF(functions\fnc_getDogtagData.sqf);
             };
             class canCheckDogtag {
                 file = QPATHTOF(functions\fnc_canCheckDogtag.sqf);
             };
         };
     };
-	class overwrite_medical_treatment {
+    class overwrite_medical_treatment {
 		tag = "ace_medical_treatment";
 		class ace_medical_treatment {
-            class cprLocal {
-                file = QPATHTOF(functions\fnc_cprLocal.sqf);
-            };
+          	class cprLocal {
+            	file = QPATHTOF(functions\fnc_cprLocal.sqf);
+          	};
 			class treatment {
-                file = QPATHTOF(functions\fnc_treatment.sqf);
-            };
-        };
+            	file = QPATHTOF(functions\fnc_treatment.sqf);
+          	};
+      	};
 	};
 };
