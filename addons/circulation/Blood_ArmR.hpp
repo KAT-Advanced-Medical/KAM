f243ca4c5ae27ac9c59b82b9c813e1618f5529b9
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2022-04-22 14:06:40 +0200

    DisplayName

--- addons/circulation/Blood_ArmR.hpp
+++ addons/circulation/Blood_ArmR.hpp
@@ -1,123 +1,123 @@
 class BloodIV;
 class BloodIV_O_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_O);
+    displayName = CSTRING(Action_BloodIV_O_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_O_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_O);
+    displayName = CSTRING(Action_BloodIV_O_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_A);
+    displayName = CSTRING(Action_BloodIV_A_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_A);
+    displayName = CSTRING(Action_BloodIV_A_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_B);
+    displayName = CSTRING(Action_BloodIV_B_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_B);
+    displayName = CSTRING(Action_BloodIV_B_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_AB);
+    displayName = CSTRING(Action_BloodIV_AB_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_AB);
+    displayName = CSTRING(Action_BloodIV_AB_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_500;
 class BloodIV_O_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_O);
+    displayName = CSTRING(Action_500_BloodIV_O_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_500_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_500_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_O_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_O);
+    displayName = CSTRING(Action_500_BloodIV_O_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_500_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_500_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_A);
+    displayName = CSTRING(Action_500_BloodIV_A_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_500_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_500_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_A);
+    displayName = CSTRING(Action_500_BloodIV_A_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_500_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_500_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_B);
+    displayName = CSTRING(Action_500_BloodIV_B_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_500_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_500_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_B);
+    displayName = CSTRING(Action_500_BloodIV_B_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_500_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_500_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_AB);
+    displayName = CSTRING(Action_500_BloodIV_AB_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_500_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_500_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_AB);
+    displayName = CSTRING(Action_500_BloodIV_AB_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_500_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_500_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_250;
 class BloodIV_O_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_O);
+    displayName = CSTRING(Action_250_BloodIV_O_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_250_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_250_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_O_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_O);
+    displayName = CSTRING(Action_250_BloodIV_O_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_O_250_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_O_250_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_A);
+    displayName = CSTRING(Action_250_BloodIV_A_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_250_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_250_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_A_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_A);
+    displayName = CSTRING(Action_250_BloodIV_A_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_A_250_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_A_250_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_B);
+    displayName = CSTRING(Action_250_BloodIV_B_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_250_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_250_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_B_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_B);
+    displayName = CSTRING(Action_250_BloodIV_B_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_B_250_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_B_250_RhN'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_AB);
+    displayName = CSTRING(Action_250_BloodIV_AB_RhP);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_250_RhP'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_250_RhP'] call ace_medical_treatment_fnc_treatment";
 };
 class BloodIV_AB_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_AB);
+    displayName = CSTRING(Action_250_BloodIV_AB_RhN);
     condition = "[_player, _target, 'hand_l', 'BloodIV_AB_250_RhN'] call ace_medical_treatment_fnc_canTreatCached";
     statement = "[_player, _target, 'hand_l', 'BloodIV_AB_250_RhN'] call ace_medical_treatment_fnc_treatment";
 };
