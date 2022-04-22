1e90dda87d0c4036a5df5fdca5f92ff515241f65
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2022-04-22 22:42:40 +0200

    Update Blood_Medical.hpp

--- addons/circulation/Blood_Medical.hpp
+++ addons/circulation/Blood_Medical.hpp
@@ -1,124 +1,124 @@
 class BloodIV;
-class BloodIV_O_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_O_RhP);
-    items[] = {"kat_bloodIV_O_RhP"};
+class BloodIV_O: BloodIV {
+    displayName = CSTRING(Action_BloodIV_O);
+    items[] = {"kat_bloodIV_O"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_O_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_O_RhN);
-    items[] = {"kat_bloodIV_O_RhN"};
+class BloodIV_O_N: BloodIV {
+    displayName = CSTRING(Action_BloodIV_O_N);
+    items[] = {"kat_bloodIV_O_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_A_RhP);
-    items[] = {"kat_bloodIV_A_RhP"};
+class BloodIV_A: BloodIV {
+    displayName = CSTRING(Action_BloodIV_A);
+    items[] = {"kat_bloodIV_A"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_A_RhN);
-    items[] = {"kat_bloodIV_A_RhN"};
+class BloodIV_A_N: BloodIV {
+    displayName = CSTRING(Action_BloodIV_A_N);
+    items[] = {"kat_bloodIV_A_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_B_RhP);
-    items[] = {"kat_bloodIV_B_RhP"};
+class BloodIV_B: BloodIV {
+    displayName = CSTRING(Action_BloodIV_B);
+    items[] = {"kat_bloodIV_B"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_B_RhN);
-    items[] = {"kat_bloodIV_B_RhN"};
+class BloodIV_B_N: BloodIV {
+    displayName = CSTRING(Action_BloodIV_B_N);
+    items[] = {"kat_bloodIV_B_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_RhP: BloodIV {
-    displayName = CSTRING(Action_BloodIV_AB_RhP);
-    items[] = {"kat_bloodIV_AB_RhP"};
+class BloodIV_AB: BloodIV {
+    displayName = CSTRING(Action_BloodIV_AB);
+    items[] = {"kat_bloodIV_AB"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_RhN: BloodIV {
-    displayName = CSTRING(Action_BloodIV_AB_RhN);
-    items[] = {"kat_bloodIV_AB_RhN"};
+class BloodIV_AB_N: BloodIV {
+    displayName = CSTRING(Action_BloodIV_AB_N);
+    items[] = {"kat_bloodIV_AB_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
 class BloodIV_500;
-class BloodIV_O_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_O_RhP);
-    items[] = {"kat_bloodIV_O_500_RhP"};
+class BloodIV_O_500: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_O);
+    items[] = {"kat_bloodIV_O_500"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_O_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_O_RhN);
-    items[] = {"kat_bloodIV_O_500_RhN"};
+class BloodIV_O_500_N: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_O_N);
+    items[] = {"kat_bloodIV_O_500_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_A_RhP);
-    items[] = {"kat_bloodIV_A_500_RhP"};
+class BloodIV_A_500: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_A);
+    items[] = {"kat_bloodIV_A_500"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_A_RhN);
-    items[] = {"kat_bloodIV_A_500_RhN"};
+class BloodIV_A_500_N: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_A_N);
+    items[] = {"kat_bloodIV_A_500_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_B_RhP);
-    items[] = {"kat_bloodIV_B_500_RhP"};
+class BloodIV_B_500: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_B);
+    items[] = {"kat_bloodIV_B_500"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_B_RhN);
-    items[] = {"kat_bloodIV_B_500_RhN"};
+class BloodIV_B_500_N: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_B_N);
+    items[] = {"kat_bloodIV_B_500_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_500_RhP: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_AB_RhP);
-    items[] = {"kat_bloodIV_AB_500_RhP"};
+class BloodIV_AB_500: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_AB);
+    items[] = {"kat_bloodIV_AB_500"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_500_RhN: BloodIV_500 {
-    displayName = CSTRING(Action_500_BloodIV_AB_RhN);
-    items[] = {"kat_bloodIV_AB_500_RhN"};
+class BloodIV_AB_500_N: BloodIV_500 {
+    displayName = CSTRING(Action_500_BloodIV_AB_N);
+    items[] = {"kat_bloodIV_AB_500_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
 class BloodIV_250;
-class BloodIV_O_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_O_RhP);
-    items[] = {"kat_bloodIV_O_250_RhP"};
+class BloodIV_O_250: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_O);
+    items[] = {"kat_bloodIV_O_250"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_O_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_O_RhN);
-    items[] = {"kat_bloodIV_O_250_RhN"};
+class BloodIV_O_250_N: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_O_N);
+    items[] = {"kat_bloodIV_O_250_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_A_RhP);
-    items[] = {"kat_bloodIV_A_250_RhP"};
+class BloodIV_A_250: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_A);
+    items[] = {"kat_bloodIV_A_250"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_A_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_A_RhN);
-    items[] = {"kat_bloodIV_A_250_RhN"};
+class BloodIV_A_250_N: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_A_N);
+    items[] = {"kat_bloodIV_A_250_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_B_RhP);
-    items[] = {"kat_bloodIV_B_250_RhP"};
+class BloodIV_B_250: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_B);
+    items[] = {"kat_bloodIV_B_250"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_B_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_B_RhN);
-    items[] = {"kat_bloodIV_B_250_RhN"};
+class BloodIV_B_250_N: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_B_N);
+    items[] = {"kat_bloodIV_B_250_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_250_RhP: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_AB_RhP);
-    items[] = {"kat_bloodIV_AB_250_RhP"};
+class BloodIV_AB_250: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_AB);
+    items[] = {"kat_bloodIV_AB_250"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
-class BloodIV_AB_250_RhN: BloodIV_250 {
-    displayName = CSTRING(Action_250_BloodIV_AB_RhN);
-    items[] = {"kat_bloodIV_AB_250_RhN"};
+class BloodIV_AB_250_N: BloodIV_250 {
+    displayName = CSTRING(Action_250_BloodIV_AB_N);
+    items[] = {"kat_bloodIV_AB_250_N"};
     callbackSuccess = QUOTE(_this call FUNC(handleTreatment));
 };
 
