4cdb30de871526d487e7dd9060d6254614b2da0b
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2023-01-17 22:09:13 +0100

    alphabetical order

--- addons/circulation/XEH_PREP.hpp
+++ addons/circulation/XEH_PREP.hpp
@@ -25,6 +25,6 @@ PREP(removeLog);
 PREP(returnAED_X);
 PREP(treatment);
 PREP(treatmentAdvanced_IV);
+PREP(updateInternalBleeding);
 PREP(vehicleCheck);
 PREP(wrongBloodTreatment);
-PREP(updateInternalBleeding);
