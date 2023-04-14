27e3bf9b8e96b1ea04973d74a2395bf9b821cc2c
fijik1124 <business.galaxyside.eu@gmail.com>
2023-04-14 22:26:43 +0200

    Add model/config
    Add kat_iv.p3d
    Add model.cfg

--- addons/pharma/CfgWeapons.hpp
+++ addons/pharma/CfgWeapons.hpp
@@ -44,7 +44,7 @@ class CfgWeapons {
         scope = 2;
         displayName = CSTRING(IV_16_Display);
         picture = QPATHTOF(ui\icon_IV_16.paa);
-        model = "\A3\Structures_F_EPA\Items\Medical\Painkillers_F.p3d";
+        model = QPATHTOF(models\kat_iv.p3d);
         descriptionShort = CSTRING(IV_DescShort);
         hiddenSelections[]={"0"};
         hiddenSelectionsMaterials[] = {QPATHTOF(models\iv\kat_iv.rvmat)};
