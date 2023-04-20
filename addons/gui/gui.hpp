9db830cddc0975055eac1e522dba2327684ae3fc
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2023-04-20 11:19:47 +0200

    PostRelease - Fix for Medical GUI (#290)

--- addons/gui/gui.hpp
+++ addons/gui/gui.hpp
@@ -7,13 +7,13 @@ class RscButtonMenu;
 class RscControlsGroupNoScrollbars;
 
 class ace_medical_gui_TriageToggle: RscButton {
-    x = "POS_X(14.33)";
+    x = QUOTE(POS_X(14.33));
     w = "POS_W(10.3)";
 };
 
 class ace_medical_gui_TriageSelect: RscControlsGroupNoScrollbars {
-    x = "POS_X(14.33)";
-    w = "POS_W(10.3)";
+    x = QUOTE(POS_X(14.33));
+    w = QUOTE(POS_W(10.3));
     class controls {
         class None: RscButton {
             w = "POS_W(10.3)";
@@ -29,20 +29,20 @@ class ACE_Medical_Menu {
             onButtonClick = QUOTE(ace_medical_gui_selectedCategory = 'surgery');
             text = QPATHTOF(data\categories\plate.paa);
             tooltip = "Surgery";
-            x = "POS_X(13.5)";
+            x = QUOTE(POS_X(13.5));
         };
         class Action1: RscButtonMenu {};
         class Action10: Action1 {
             idc = IDC_ACTION_10;
-            y = "POS_Y(14.4)";
+            y = QUOTE(POS_Y(14.4));
         };
         class Action11: Action1 {
             idc = IDC_ACTION_11;
-            y = "POS_Y(15.5)";
+            y = QUOTE(POS_Y(15.5));
         };
         class TriageStatus: RscText {
-            x = "POS_X(14.33)";
-            w = "POS_W(10.33)";
+            x = QUOTE(POS_X(14.33));
+            w = QUOTE(POS_W(10.33));
         };
     };
 };
