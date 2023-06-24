850a804419e775bf1c14c9666c1da43ce0686c73
Mia <95135995+MissHeda@users.noreply.github.com>
2023-06-24 21:10:03 +0200

    Misc | Add | Blood Group Patches (#351)
    **When merged this pull request will:**
    - Add Blood Group Patches (Unit Insignia)
    
    ![20230622073435_1](https://github.com/KAT-Advanced-Medical/KAM/assets/95135995/100da1fa-288c-4063-bfcc-f59d98715c34)
    
    ### IMPORTANT
    - [Development Guidelines](https://ace3.acemod.org/wiki/development/)
    are read, understood and applied.
    - Title of this PR uses our standard template `Component -
    Add|Fix|Improve|Change|Make|Remove {changes}`.

---
+++ addons/misc/CfgBloodGroupsInsignia.hpp
@@ -0,0 +1,50 @@
+class CfgUnitInsignia {
+    class kat_BloodGroupPatch_Red_AP {
+        displayName = CSTRING(BloodGroupPatch_A_P);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\A_Pos_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_AN {
+        displayName = CSTRING(BloodGroupPatch_A_N);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\A_Neg_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_BP {
+        displayName = CSTRING(BloodGroupPatch_B_P);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\B_Pos_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_BN {
+        displayName = CSTRING(BloodGroupPatch_B_N);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\B_Neg_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_0P {
+        displayName = CSTRING(BloodGroupPatch_0_P);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\0_Pos_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_0N {
+        displayName = CSTRING(BloodGroupPatch_0_N);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\0_Neg_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_ABP {
+        displayName = CSTRING(BloodGroupPatch_AB_P);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\AB_Pos_Red.paa);
+        textureVehicle = "";
+    };
+    class kat_BloodGroupPatch_Red_ABN {
+        displayName = CSTRING(BloodGroupPatch_AB_N);
+        author = "Miss Heda";
+        texture = QPATHTOF(ui\BloodGroupPatches\AB_Neg_Red.paa);
+        textureVehicle = "";
+    };
+};
