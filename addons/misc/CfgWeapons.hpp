d0adbe062d9fdc264ef99a5b57182afbe23d1917
YetheSamartaka <55753928+YetheSamartaka@users.noreply.github.com>
2023-06-24 22:19:43 +0200

    Misc - Fix missing class declaration (#353)
    **When merged this pull request will:**
    - _Fix missing class ACE_ItemCore declaration_
    - _Fix missing class CBA_MiscItem_ItemInfo declaration and usage in
    respective items_
    - _Remove unused class ItemCore_
    
    ### IMPORTANT
    
    - [Development Guidelines](https://ace3.acemod.org/wiki/development/)
    are read, understood and applied.
    - Title of this PR uses our standard template `Component -
    Add|Fix|Improve|Change|Make|Remove {changes}`.

--- addons/misc/CfgWeapons.hpp
+++ addons/misc/CfgWeapons.hpp
@@ -1,14 +1,14 @@
 class CfgWeapons
 {
     class ToolKit;
-    class Attachable_Helistretcher: ToolKit
-    {
+    class ACE_ItemCore;
+    class CBA_MiscItem_ItemInfo;
+    class Attachable_Helistretcher: ToolKit {
         author = "Battlekeeper";
         picture = QPATHTOF(ui\stretcher.paa);
         displayName = CSTRING(Helistretcher_Display);
         descriptionShort = CSTRING(Helistretcher_Desc);
-        class ItemInfo
-        {
+        class ItemInfo {
             mass = 20;
             uniformModel = "\A3\Weapons_F\Items\Toolkit";
             type = 620;
@@ -17,50 +17,44 @@ class CfgWeapons
         };
     };
 
-    class ItemCore;
     class kat_Armband_Red_Cross_Item: ACE_ItemCore {
-
         author = "Miss Heda, vccv9040 (Swedish Forces Pack)";
         displayName = CSTRING(Armband_Red_Cross_ItemName);
         scope = 2;
         model = QPATHTOF(models\armband\armband.p3d);
         editorPreview = QPATHTOF(ui\ArmbandWhiteCross.paa);
         picture = QPATHTOF(ui\ArmbandWhiteCross.paa);
-        class ItemInfo: InventoryItem_Base_F {
+        class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 3;
         };
     };
 
     class kat_Armband_Medic_Item: kat_Armband_Red_Cross_Item {
-
         displayName = CSTRING(Armband_Medic_ItemName);
         editorPreview = QPATHTOF(ui\ArmbandWhiteCrossMedic.paa);
         picture = QPATHTOF(ui\ArmbandWhiteCrossMedic.paa);
     };
 
     class kat_Armband_Doctor_Item: kat_Armband_Red_Cross_Item {
-
         displayName = CSTRING(Armband_Doctor_ItemName);
         editorPreview = QPATHTOF(ui\ArmbandWhiteCrossDoctor.paa);
         picture = QPATHTOF(ui\ArmbandWhiteCrossDoctor.paa);
     };
 
     class kat_Armband_Kat_Item: kat_Armband_Red_Cross_Item {
-
         displayName = CSTRING(Armband_Kat_ItemName);
         editorPreview = QPATHTOF(ui\ArmbandKat.paa);
         picture = QPATHTOF(ui\ArmbandKat.paa);
     };
 
     class kat_Bubble_Wrap_Item: ACE_ItemCore {
-
         author = "Miss Heda";
         displayName = CSTRING(Bubble_Wrap_Display);
         descriptionShort = CSTRING(Bubble_Wrap_Desc);
         scope = 2;
         editorPreview = QPATHTOF(ui\Bubble_Wrap_Item.paa);
         picture = QPATHTOF(ui\Bubble_Wrap_Item.paa);
-        class ItemInfo: InventoryItem_Base_F {
+        class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 0.1;
         };
     };
