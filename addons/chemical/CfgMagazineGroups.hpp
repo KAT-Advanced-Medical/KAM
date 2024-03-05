0549b73834467d2d0698c991df0f054dd1e99f38
OlafZett <133373192+OlafZett@users.noreply.github.com>
2024-03-05 21:16:14 +0100

    Delete addons/chemical directory

--- addons/chemical/CfgMagazineGroups.hpp
+++
@@ -1,9 +0,0 @@
-//class ace_csw_groups;
-class ace_csw_groups {
-    class KAT_1Rnd_82mm_Mo_Type0 {
-        KAT_1Rnd_82mm_Mo_Type0 = 1;
-    };
-    class KAT_1Rnd_82mm_Mo_TypeCS {
-        KAT_1Rnd_82mm_Mo_TypeCS = 1;
-    };
-};
