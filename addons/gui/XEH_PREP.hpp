2ac0838f3d0319e5aeb16125474e6270a3e04051
OlafZett <133373192+OlafZett@users.noreply.github.com>
2024-03-05 21:16:26 +0100

    Delete addons/gui directory

--- addons/gui/XEH_PREP.hpp
+++
@@ -1,9 +0,0 @@
-PREP(countTreatmentItems);
-PREP(displayPatientInformation);
-PREP(onMenuClose);
-PREP(onMenuOpen);
-PREP(updateActions);
-PREP(updateBodyImage);
-PREP(updateCategories);
-PREP(updateInjuryList);
-PREP(updateLogList);
