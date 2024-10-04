847b9d700944a6c87e5a0176d67811540e937654
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2024-10-04 10:23:25 -0700

    Forgot atropine

--- addons/pharma/CfgWeapons.hpp
+++ addons/pharma/CfgWeapons.hpp
@@ -674,4 +674,26 @@ class CfgWeapons {
             mass = 1;
         };
     };
+    class kat_syringe_atropine_5ml_1: ACE_ItemCore {
+        scope = 1;
+        displayName = CSTRING(Syringe_atropine_5_1_Display);
+        picture = QPATHTOF(ui\syringe_item.paa);
+        model = "\A3\Structures_F_EPA\Items\Medical\Painkillers_F.p3d";
+        descriptionShort = CSTRING(Syringe_atropine_5_1_DescShort);
+        ACE_isMedicalItem = 1;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
+    class kat_syringe_atropine_5ml_3: ACE_ItemCore {
+        scope = 1;
+        displayName = CSTRING(Syringe_atropine_5_3_Display);
+        picture = QPATHTOF(ui\syringe_item.paa);
+        model = "\A3\Structures_F_EPA\Items\Medical\Painkillers_F.p3d";
+        descriptionShort = CSTRING(Syringe_atropine_5_3_DescShort);
+        ACE_isMedicalItem = 1;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
 };
