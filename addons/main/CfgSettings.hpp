8afec192edcc21165c172043fad75ad4a18b7afd
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2024-10-08 18:00:52 +0200

    Update CfgSettings.hpp

--- addons/main/CfgSettings.hpp
+++ addons/main/CfgSettings.hpp
@@ -3,7 +3,7 @@ class CfgSettings {
         class Versioning {
             class PREFIX {
                 class dependencies {
-                    //ACE will hard exit if this is missing
+                    //KAT will hard exit if this is missing
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "(true)"};
                     ACE[] = {"ace_main", REQUIRED_ACE_VERSION, "(true)"};
                 };
