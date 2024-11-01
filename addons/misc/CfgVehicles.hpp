aa051247ddfc1ee56bf4924e8cff2c9bc5d7f52b
YetheSamartaka <55753928+YetheSamartaka@users.noreply.github.com>
2024-11-01 22:10:02 +0100

    removed some code
    no idea how it got there, I was just getting branches synced

--- addons/misc/CfgVehicles.hpp
+++ addons/misc/CfgVehicles.hpp
@@ -1259,8 +1259,6 @@ class CfgVehicles {
 
     class Car_F: Car {};
     class Quadbike_01_base_F: Car_F {
-        stretcherPos[] = {0,0.8,-0.56};
-        stretcherVector[] = {{1, 0, 0}, {0, 0, 1}};
         class ACE_Actions: ACE_Actions {
             class ACE_MainActions: ACE_MainActions {
                 class KAT_UnloadAndCarryPatient {
