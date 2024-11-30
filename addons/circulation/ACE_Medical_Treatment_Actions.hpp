0bd49d34afd1a344e90e6795ee4c6803f40e9807
mazinskihenry <33608576+mazinskihenry@users.noreply.github.com>
2024-11-29 16:56:30 -0800

    Update ACE_Medical_Treatment_Actions.hpp

--- addons/circulation/ACE_Medical_Treatment_Actions.hpp
+++ addons/circulation/ACE_Medical_Treatment_Actions.hpp
@@ -67,6 +67,7 @@ class ACE_Medical_Treatment_Actions {
         displayName = CSTRING(DrawArterial_Action_Use);
         displayNameProgress = CSTRING(DrawBlood_Action_Progress);
         treatmentTime = 10;
+        condition = QUOTE([ARR_3(_medic,_patient,500)] call FUNC(canDraw) && (!(EGVAR(pharma,RequireInsIVBloodDraw)) || EFUNC(pharma,removeIV)) && GVAR(abgEnable));
         callbackSuccess = QFUNC(drawArterial);
         condition = "true";
         items[] = {"kat_IV_16"};
