8045103b8d28c7d92c734a65351b4d16739172a6
mazinskihenry <33608576+mazinskihenry@users.noreply.github.com>
2025-01-07 22:09:45 -0800

    Icon

--- addons/ophthalmology/CfgWeapons.hpp
+++ addons/ophthalmology/CfgWeapons.hpp
@@ -6,7 +6,7 @@ class CfgWeapons
     class kat_eyecovers: ACE_ItemCore {
         scope = 2;
         displayName = CSTRING(EyeCover_Item);
-        picture = QPATHTOF(ui\maskSOLR.paa);
+        picture = QPATHTOF(ui\icon_eyeshield.paa);
         model = QPATHTOF(models\eyecovers\kat_eyecovers.p3d);
         descriptionShort = CSTRING(EyeCover_Desc);
         ACE_isMedicalItem = 1;
@@ -19,13 +19,13 @@ class CfgWeapons
     class kat_eyecovers_right: NVGoggles
     {
         author = "MrAdrianPL";
-        displayName = CSTRING(EyeCover_Right);
+        displayName = CSTRING(EyeCover_Item);
         scope = 2;
         scopeArsenal = 0;
         ace_nightvision_border = QPATHTOF(ui\RightEyeNVG.paa);
         modelOptics = "\A3\Weapons_F\empty.p3d";
 
-        picture = QPATHTOF(ui\maskSOLR.paa);
+        picture = QPATHTOF(ui\icon_eyeshield.paa);
         hiddenSelections[] = {"eye_l","eye_r"};
         hiddenSelectionsTextures[] = {"",QPATHTOF(models\eyecovers\camo1_ca.paa)};
         visionMode[] = {};
@@ -40,7 +40,7 @@ class CfgWeapons
     };
     class kat_eyecovers_left: kat_eyecovers_right
     {
-        displayName = CSTRING(EyeCover_Left);
+        displayName = CSTRING(EyeCover_Item);
         ace_nightvision_border = QPATHTOF(ui\LeftEyeNVG.paa);
         modelOptics = "\A3\Weapons_F\empty.p3d";
         hiddenSelections[] = {"eye_l","eye_r"};
