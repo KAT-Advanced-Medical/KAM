5c4e963190ac8cd952bbd3da26c5c77e47c3938b
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2025-01-15 20:09:04 +0100

    Update CfgWeapons.hpp

--- addons/vitals/CfgWeapons.hpp
+++ addons/vitals/CfgWeapons.hpp
@@ -6,8 +6,8 @@ class CfgWeapons {
         author = "Mazinski";
         displayName = CSTRING(basicDiagnostic_display);
         descriptionShort = CSTRING(basicDiagnostic_description);
-        picture = QPATHTOF(ui\basicDiagnostic.paa); // FLAG
-        icon = "";
+        picture = QPATHTOF(ui\basicDiagnostic.paa);
+        ACE_isMedicalItem = 1;
         class ItemInfo: CBA_MiscItem_ItemInfo {
             mass = 8;
         };
