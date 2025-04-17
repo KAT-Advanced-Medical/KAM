c5e1b30a0f8fcf69815da7576f733ac1f03bf677
Apricot <50947830+Apricot-ale@users.noreply.github.com>
2025-04-18 07:01:25 +0900

    Tweak Surgery Box
    increase item counts
    Remove empty line

--- addons/surgery/CfgVehicles.hpp
+++ addons/surgery/CfgVehicles.hpp
@@ -17,18 +17,17 @@ class CfgVehicles {
             MACRO_ADDITEM(kat_reboa,10);
         };
     };
-
     class kat_surgerySupplyCrate: ACE_medicalSupplyCrate {
         displayName = CSTRING(surgeryToolbox);
         class TransportItems {
             MACRO_ADDITEM(kat_etomidate,15);
             MACRO_ADDITEM(kat_flumazenil,15);
             MACRO_ADDITEM(kat_lorazepam,15);
-            MACRO_ADDITEM(kat_clamp,3);
+            MACRO_ADDITEM(kat_clamp,5);
             MACRO_ADDITEM(kat_plate,15);
-            MACRO_ADDITEM(kat_retractor,3);
-            MACRO_ADDITEM(kat_scalpel,3);
-            MACRO_ADDITEM(kat_vacuum,3);
+            MACRO_ADDITEM(kat_retractor,5);
+            MACRO_ADDITEM(kat_scalpel,5);
+            MACRO_ADDITEM(kat_vacuum,15);
         };
     };
 };
