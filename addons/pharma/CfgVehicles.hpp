7bfb538069bca6d03160e15233b599e33ad3be03
Apricot <50947830+Apricot-ale@users.noreply.github.com>
2025-04-18 06:04:47 +0900

    sort pharma
    increase carbonate counts too

--- addons/pharma/CfgVehicles.hpp
+++ addons/pharma/CfgVehicles.hpp
@@ -27,30 +27,30 @@ class CfgVehicles {
 
     class ACE_medicalSupplyCrate_advanced: ACE_medicalSupplyCrate {
         class TransportItems {
-            MACRO_ADDITEM(kat_amiodarone,20);
-            MACRO_ADDITEM(kat_atropine,20);
-            MACRO_ADDITEM(kat_IO_FAST,20);
             MACRO_ADDITEM(kat_IV_16,20);
-            MACRO_ADDITEM(kat_lidocaine,20);
+            MACRO_ADDITEM(kat_IO_FAST,20);
             MACRO_ADDITEM(kat_naloxone,20);
-            MACRO_ADDITEM(kat_nitroglycerin,20);
+            MACRO_ADDITEM(kat_EACA,20);
+            MACRO_ADDITEM(kat_TXA,20);
             MACRO_ADDITEM(kat_norepinephrine,20);
             MACRO_ADDITEM(kat_phenylephrine,20);
             MACRO_ADDITEM(kat_phenylephrineAuto,20);
-            MACRO_ADDITEM(kat_TXA,20);
+            MACRO_ADDITEM(kat_nitroglycerin,20);
+            MACRO_ADDITEM(kat_amiodarone,20);
+            MACRO_ADDITEM(kat_lidocaine,20);
+            MACRO_ADDITEM(kat_atropine,20);
             MACRO_ADDITEM(kat_ketamine,20);
             MACRO_ADDITEM(kat_fentanyl,20);
             MACRO_ADDITEM(kat_nalbuphine,20);
             MACRO_ADDITEM(kat_lorazepam,20);
             MACRO_ADDITEM(kat_flumazenil,20);
             MACRO_ADDITEM(kat_etomidate,20);
-            MACRO_ADDITEM(kat_EACA,20);
             MACRO_ADDITEM(kat_epinephrineIV,20);
             MACRO_ADDITEM(kat_coag_sense,2);
         };
         class TransportMagazines {
-            MACRO_ADDMAGAZINE(kat_Carbonate,10);
             MACRO_ADDMAGAZINE(kat_Painkiller,20);
+            MACRO_ADDMAGAZINE(kat_Carbonate,15);
             MACRO_ADDMAGAZINE(kat_Pervitin,10);
             MACRO_ADDMAGAZINE(kat_Caffeine,15);
             MACRO_ADDMAGAZINE(kat_Penthrox,15);
