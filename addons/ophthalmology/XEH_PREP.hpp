bd925299eb02c4652c0fc5f34e5f53bd44d68de4
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2025-05-10 08:03:57 -0700

    Revert "Opthomalogy fixes"
    This reverts commit c455b5b40589c301074f749c87b7091fc9a697ac.

--- addons/ophthalmology/XEH_PREP.hpp
+++
@@ -1,9 +0,0 @@
-PREP(eyeShieldCondition);
-PREP(fullHealLocal);
-PREP(gui_updateInjuryListPart);
-PREP(handleDustInjury);
-PREP(handleExplosion);
-PREP(handleRespawn);
-PREP(init);
-PREP(treatmentAdvanced_eyeShield);
-PREP(treatmentAdvanced_eyewash);
