ebc077eb913b064307dde4cbf6a3ca3807baf590
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2025-05-26 15:28:56 -0700

    Update config.cpp

--- addons/hitpoints/config.cpp
+++ addons/hitpoints/config.cpp
@@ -3,6 +3,7 @@
 class CfgPatches {
     class ADDON    {
         name = COMPONENT_NAME;
+        requiredVersion = REQUIRED_VERSION;
         units[] = {};
         weapons[] = {};
         magazines[] = { };
