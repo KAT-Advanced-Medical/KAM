8054cec8bebc8a917c9054fdae0855ab6e10aa86
Cplhardcore <135324281+Cplhardcore@users.noreply.github.com>
2026-02-28 15:58:59 -0800

    Update script_macros.hpp

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -404,6 +404,7 @@
 // Pharma
 #define VAR_VASOCONSTRICTION           QEGVAR(pharma,alphaAction)
 #define GET_VASOCONSTRICTION(unit)     (unit getVariable [VAR_VASOCONSTRICTION, [1,1,1,1,1,1,1,1,1,1,1,1]])
+#define GET_LOCAL_VASOCONSTRICTION(unit,partindex)      ((unit getVariable [VAR_LOCAL_ANESTHESIA, [1,1,1,1,1,1,1,1,1,1,1,1]]) select _partindex)
 
 #define VAR_LOCAL_ANESTHESIA            QEGVAR(pharma,localAnesthesia)
 #define GET_LOCAL_ANESTHESIA(unit,partindex)      ((unit getVariable [VAR_LOCAL_ANESTHESIA, DEFAULT_LOCAL_ANESTHESIA]) select _partindex)
